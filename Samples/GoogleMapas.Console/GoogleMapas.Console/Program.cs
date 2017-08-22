b41a590ba14b7d75a874a79a2bb828bbe0716c17
Rodrigo Mendonça da Paixão <rodrigo-mendonca@outlook.com.br>
2017-08-22 17:43:40 -0300

    Ëxemplo de calculo de rotas usando o google mapas

---
+++ Samples/GoogleMapas.Console/GoogleMapas.Console/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using PathFinder.Routes;
+
+namespace GoogleMapas.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var search = new SearchRoute();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("1 Route");
+
+            var route = search.GetRoute("São Paulo", "Rio de Janeiro");
+	        ShowRoute(route);				
+
+            Console.WriteLine();
+            Console.WriteLine("Multi Routes");
+
+            var routes = search.GetRoutes("Curitiba", "São Paulo", "Rio de Janeiro", "Belo Horizonte", "Vitória", "Salvador BA", "Fortaleza");
+
+            foreach (var item in routes)
+                ShowRoute(item);
+
+            Console.ReadKey();
+        }
+
+        static void ShowRoute(Route route)
+        {
+            if (route.Sucess)
+            {
+                var disformat = string.Format("{0:n2} km", route.Km);
+                var durformat = string.Format("{0:n2} horas)", route.Hours);
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine($"Origem: {route.Origin} Destino: {route.Destination} Distancia: {disformat} Duração: {durformat}");
+            }
+            else 
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Origem: {route.Origin} Destino: {route.Destination} - Erro!");
+            }
+        }
+    }
+}
---
+++ Samples/GoogleMapas.Console/PathFinder.Routes/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PathFinder.Routes
+{
+    public class Class1
+    {
+    }
+}
