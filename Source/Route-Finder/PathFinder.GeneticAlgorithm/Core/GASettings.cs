64d609918e7b87661e5df0abc6deb3d44ae1d2ad
rodrigo-mendonca <rodrigo-mendonca@outlook.com.br>
2018-04-29 23:25:11 -0300

    Correção do merge

--- Source/Route-Finder/PathFinder.GeneticAlgorithm/Core/GASettings.cs
+++ Source/Route-Finder/PathFinder.GeneticAlgorithm/Core/GASettings.cs
@@ -10,14 +10,8 @@
         public double CrossoverRate { get; set; } = 0.5;
         public double MutationRate { get; set; } = 0.001;
 
-<<<<<<< HEAD
         public int PopulationSize { get; set; } = 500;
         public int GenerationLimit { get; set; } = 50;
         public int BestSolutionToPick { get; set; } = 2;
-=======
-        public int PopulationSize { get; set; } = 100;
-        public int GenerationLimit { get; set; } = 20;
-        public int BestSolutionToPick { get; set; } = 1;
->>>>>>> 0cdf3b9ae6360a1d78c542d69b7d875f8f7f81ea
     }
 }
