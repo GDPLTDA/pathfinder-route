37b6fbe693bce4c3b78a0abbca03477fec7d5de8
Lucas Teles <lucas.teles@lambda3.com.br>
2018-04-30 02:18:40 -0300

    adicao de mutation

---
+++ Source/Route-Finder/PathFinder.GeneticAlgorithm/Mutation/InversionMutation.cs
@@ -0,0 +1,37 @@
+﻿using PathFinder.GeneticAlgorithm.Abstraction;
+using System.Linq;
+
+namespace PathFinder.GeneticAlgorithm.Mutation
+{
+    public class InversionMutation : AbstractMutate
+    {
+        private readonly IRandom random;
+
+        public InversionMutation(GASettings settings, IRandom random) : base(settings)
+        {
+            this.random = random;
+        }
+
+        public override Genome Apply(Genome baby)
+        {
+
+            if (random.NextDouble() > MutationRate)
+                return baby;
+
+            var truck = baby.Trucks[random.Next(0, baby.GetUsedTrucksCount)];
+            var localIndex = random.Next(0, truck.Locals.Count);
+
+            var count = random.Next(0, truck.Locals.Count - localIndex);
+
+            var subroute = truck.Locals.Skip(localIndex).Take(count).Reverse();
+
+            truck.Locals = truck.Locals
+                                .Take(localIndex)
+                                .Concat(subroute)
+                                .Concat(truck.Locals.Skip(localIndex + count))
+                                .ToList();
+
+            return baby;
+        }
+    }
+}
---
+++ Source/Route-Finder/PathFinder.GeneticAlgorithm/Mutation/SwapMutation.cs
@@ -0,0 +1,33 @@
+﻿using PathFinder.GeneticAlgorithm.Abstraction;
+
+namespace PathFinder.GeneticAlgorithm.Mutation
+{
+    public class SwapMutattion : AbstractMutate
+    {
+        private readonly IRandom random;
+
+        public SwapMutattion(GASettings settings, IRandom random) : base(settings)
+        {
+            this.random = random;
+        }
+
+        public override Genome Apply(Genome baby)
+        {
+
+            if (random.NextDouble() > MutationRate)
+                return baby;
+
+            var indexTruck1 = random.Next(0, baby.GetUsedTrucksCount);
+            var indexTruck2 = random.Next(0, baby.GetUsedTrucksCount);
+
+            var localIndex1 = random.Next(0, baby.Trucks[indexTruck1].Locals.Count);
+            var localIndex2 = random.Next(0, baby.Trucks[indexTruck2].Locals.Count);
+
+            var temp = baby.Trucks[indexTruck1].Locals[localIndex1];
+
+            baby.Trucks[indexTruck1].Locals[localIndex1] = baby.Trucks[indexTruck2].Locals[localIndex2];
+            baby.Trucks[indexTruck2].Locals[localIndex2] = temp;
+            return baby;
+        }
+    }
+}
